b4a40e3e18634190ea995dbed6dcc91a94a2b055
Sergey Koshechkin <serge.koshechkin@gmail.com>
2017-04-20 23:17:58 +0300

    (PhoneFormComponent) chore: add bootstrap containers and rows, use Observable to watch form

--- src/app/phone-form/phone-form.component.ts
+++ src/app/phone-form/phone-form.component.ts
@@ -1,7 +1,7 @@
-import { Component, OnInit } from '@angular/core';
+import { ChangeDetectionStrategy, Component, OnInit } from '@angular/core';
 import { FormBuilder, FormGroup } from '@angular/forms';
 import { TypesService } from "../shared/model/type/types.service";
-import { PhoneType } from "../shared/constants";
+import {PhoneState, PhoneType} from "../shared/constants";
 import { ColorsService } from "../shared/model/color/colors.service";
 import { SizesService } from "../shared/model/size/sizes.service";
 import { StatesService } from "../shared/model/state/states.service";
@@ -11,36 +11,58 @@ import { IState } from "../shared/model/state/istate";
 import { IColor } from "../shared/model/color/icolor";
 import { ISize } from "../shared/model/size/isize";
 
+
+export interface IAvailable {
+  sizes: ISize[],
+  colors: IColor[],
+  states: IState[],
+}
+
 @Component({
   selector: 'app-phone-form',
   templateUrl: './phone-form.component.html',
-  styleUrls: ['./phone-form.component.css']
+  styleUrls: ['./phone-form.component.css'],
+  // changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export class PhoneFormComponent implements OnInit {
   phoneGroupForm: FormGroup;
+  types: IType[];
   private chosenType: IType;
   private chosenPhone: IPhone;
+  available: IAvailable;
+  descriptions: string[];
   constructor(
     private formBuilder: FormBuilder,
     private typesService: TypesService,
     private colorsService: ColorsService,
     private sizesService: SizesService,
     private statesService: StatesService,
-  ) {}
+  ) {
+    this.types = this.typesService.getTypes();
+  }
 
   ngOnInit() {
     this.setNewType(PhoneType.I4S);
+    this.setDescriptions(this.chosenPhone.state);
+
     this.phoneGroupForm = this.formBuilder.group(this.chosenPhone);
-    this.phoneGroupForm.valueChanges.subscribe((data: IPhone) => {
-      if (this.chosenPhone.type !== data.type) {
-        this.setNewType(data.type);
-        this.phoneGroupForm.patchValue(this.chosenPhone);
-      } else {
-        this.chosenPhone = data;
-      }
+    this.phoneGroupForm.get('type').valueChanges.subscribe((type: PhoneType) => {
+      this.setNewType(type);
+      const newChosenPhone = Object.assign(this.chosenPhone);
+      delete newChosenPhone.type;
+      this.phoneGroupForm.patchValue(newChosenPhone, {onlySelf: true, emitEvent: false});
     });
+    ['size', 'color', 'state'].forEach(key => {
+      this.phoneGroupForm.get(key).valueChanges.subscribe((data) => {
+        this.chosenPhone = Object.assign(this.chosenPhone, {[key]: data});
+      });
+    });
+    this.phoneGroupForm.get('state').valueChanges.subscribe(
+      state => this.setDescriptions(state)
+    );
+    this.phoneGroupForm.valueChanges.subscribe(form => console.log(form));
   }
-  setNewType(newType: PhoneType) {
+  private setNewType(newType: PhoneType) {
     this.chosenType = this.typesService.getType(newType);
     this.chosenPhone = {
       type: this.chosenType.type,
@@ -48,21 +70,13 @@ export class PhoneFormComponent implements OnInit {
       color: this.chosenType.availableColors[0],
       state: this.chosenType.availableStates[0],
     };
+    this.available = {
+      sizes: this.sizesService.getSizes(this.chosenType.availableSizes),
+      colors: this.colorsService.getColors(this.chosenType.availableColors),
+      states: this.statesService.getStates(this.chosenType.availableStates),
+    };
   }
-  get types() {
-    return this.typesService.getTypes();
-  }
-  get availableSizes(): ISize[] {
-    return this.chosenType.availableSizes.map(size => this.sizesService.getSize(size));
-  }
-  get availableColors(): IColor[] {
-    return this.chosenType.availableColors.map(color => this.colorsService.getColor(color));
-  }
-  get availableStates(): IState[] {
-    return this.chosenType.availableStates.map(state => this.statesService.getState(state));
-  }
-  get descriptions(): string[] {
-    console.log("-- Again Descriptions");
-    return this.statesService.getState(this.chosenPhone.state).desc;
+  private setDescriptions(state: PhoneState) {
+    this.descriptions = this.statesService.getState(state).desc;
   }
 }
